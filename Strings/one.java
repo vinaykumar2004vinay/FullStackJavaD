86dfda3215c8d45a0f7b5e86bf849ba03848c3d3
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-12-08 19:23:57 +0530

    JAVA

---
+++ Strings/one.java
@@ -0,0 +1,11 @@
+class one
+{
+    public static void main(String[] args)
+    {
+        int number = 9;
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(number * i);
+        }
+    }
+}
---
+++ Strings/three.java
@@ -0,0 +1,11 @@
+class three
+{
+    public static void main(String[] args)
+    {
+        int num = 7;
+        for(int i=0 ; i<=10 ; i++)
+        {
+            System.out.println(" 7 " + " * " + i + " = " + num * i);
+        }
+    }
+}
